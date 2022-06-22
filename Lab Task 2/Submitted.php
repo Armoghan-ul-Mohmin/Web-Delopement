d5987533c0a069e1a81248777b5e6f2472010d40
Armoghan <74015345+Armoghan-ul-Mohmin@users.noreply.github.com>
2022-06-22 21:31:23 +0000

    Add files via upload
    Lab Task 2 Html Form and Htlm Tags

---
+++ Lab Task 2/Submitted.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab Task </title>
+</head>
+
+<body>
+    <H1>Submitted Sucessfully</H1>
+    Welcome
+    <!-- Using PHP to Get Name and Email From Index.html -->
+    <?php echo $_GET["Name"]; ?><br>
+    Your email address is:
+    <?php echo $_GET["Email"]; ?>
+</body>
+
+</html>
